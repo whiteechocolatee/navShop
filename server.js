55f35079c042fdad749a7eac70378413db94849a
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-03 18:50:30 +0300

    connect formData.parse from express-form-data package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const formData = require("express-form-data");
 
 const app = express();
 
@@ -11,12 +12,15 @@ const port = process.env.PORT || 5000;
 /* This is a chain of middleware functions. */
 app
   .use(express.json())
+  .use(formData.parse())
   .use(express.urlencoded({ extended: true }))
-  .use(express.static(`${__dirname}/assets`));
+  .use("/api/items", require("./routes/mainRoutes"));
 
 /* Connecting to the mongoDB database and then starting the server. */
-mongoose.connect(`mongodb://127.0.0.1:27017`).then(() => {
-  app.listen(port, () => {
-    console.log(`server was started at port - ${port}`);
+mongoose
+  .connect(`mongodb://127.0.0.1:27017/shop`)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server was started at port - ${port}`);
+    });
   });
-});
