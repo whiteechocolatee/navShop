562151c9beba1402d2a7a7db76ece66a91531322
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-05 14:41:17 +0300

    Create userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    require: true,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("User", userModel);
