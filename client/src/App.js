242fa4754b7bb9b5861f3c84a333462d66113368
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-06 17:44:42 +0300

    create and include bootstrap js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Button } from "./components/Button/Button";
 import { CallbackBlock } from "./components/CallbackForm/CallbackBlock";
 import { ContentWrapper } from "./components/contentWrapper/ContentWrapper";
 import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
 import { Input } from "./components/Input/Input";
 
 function App() {
   return (
     <React.Fragment>
       <ContentWrapper>
+        <Header />
         <Button children={"узнать больше"} />
         <Input placeholder={"Имя"} />
       </ContentWrapper>
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+
+export const Header = () => {
+  return (
+        <header className="navbar">
+
+        </header>
+    );
+};
