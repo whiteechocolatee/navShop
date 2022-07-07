264ab185b7885fcf211a02059a4498fdeb901487
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-07 14:31:38 +0300

    add styles to wrapper css , add new setting

--- client/src/components/contentWrapper/ContentWrapper.jsx
+++ client/src/components/contentWrapper/ContentWrapper.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import styles from "./wrapper.module.css";
 
 export const ContentWrapper = ({ className, children }) => {
   return (
-    <div className={`container-lg ${className}`}>
+    <div
+      className={`container-lg ${className} ${styles.fonts}`}>
       {children}
     </div>
   );
