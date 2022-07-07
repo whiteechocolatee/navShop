7ad041656da41dedf2275eadedd66821ee0d17b7
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-07 15:36:44 +0300

    create categories nav component

---
+++ client/src/components/CategoriesNav/CategoriesNavigation.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  RiBattery2ChargeLine,
+  RiDeviceRecoverLine,
+  RiComputerLine,
+} from "react-icons/ri";
+import {
+  GiPowerGenerator,
+  GiAudioCassette,
+} from "react-icons/gi";
+
+import styles from "./nav.module.css";
+import { ContentWrapper } from "../contentWrapper/ContentWrapper";
+
+export const CategoriesNavigation = () => {
+  return (
+    <React.Fragment>
+      <ContentWrapper>
+        <nav className={styles.nav}>
+          <ul className={styles.navBtns}>
+            <li className={styles.navBtn}>
+              <RiBattery2ChargeLine />
+              <NavLink to={`/`}>Зарядные</NavLink>
+            </li>
+            <li className={styles.navBtn}>
+              <RiDeviceRecoverLine />
+              <NavLink to={`/`}>
+                Защитные акссесуары
+              </NavLink>
+            </li>
+            <li className={styles.navBtn}>
+              <GiPowerGenerator />
+              <NavLink to={`/`}>Гаджеты</NavLink>
+            </li>
+            <li className={styles.navBtn}>
+              <GiAudioCassette />
+              <NavLink to={`/`}>Аудио</NavLink>
+            </li>
+            <li className={styles.navBtn}>
+              <RiComputerLine />
+              <NavLink to={`/`}>
+                Акссесуары для компьютеров
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </ContentWrapper>
+    </React.Fragment>
+  );
+};
