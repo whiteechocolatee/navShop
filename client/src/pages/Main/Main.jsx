ff322ee56f4c5504658f1775b126e5ca9f6bff65
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-07 15:37:21 +0300

    add categories nav to main page

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Header } from "../../components/Header/Header";
 import { CallbackBlock } from "../../components/CallbackForm/CallbackBlock";
 import { Footer } from "../../components/Footer/Footer";
+import { CategoriesNavigation } from "../../components/CategoriesNav/CategoriesNavigation";
 
 export const Main = () => {
   return (
     <React.Fragment>
       <Header />
+      <CategoriesNavigation />
       {/* <div className='fixed-bottom'> */}
       <CallbackBlock />
       <Footer />
