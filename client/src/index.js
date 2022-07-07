75fa68beadb244de0f1a38d6c0edbad5178d25b8
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-07 20:47:38 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/js/bootstrap.js";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root"),
