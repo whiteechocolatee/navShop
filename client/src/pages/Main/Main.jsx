5660c75124c11815b9716313c97f8feca00949c8
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-08 13:31:15 +0300

    Update Main.jsx

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -4,12 +4,14 @@ import { CallbackBlock } from "../../components/CallbackForm/CallbackBlock";
 import { Footer } from "../../components/Footer/Footer";
 import { CategoriesNavigation } from "../../components/CategoriesNav/CategoriesNavigation";
 import { CategoryCards } from "../../components/CategoryCard/CategoryCards";
+import { Brands } from "../../components/BrandsLine/Brands";
 
 export const Main = () => {
   return (
     <React.Fragment>
       <Header />
       <CategoriesNavigation />
+      <Brands />
       <CategoryCards />
       <CallbackBlock />
       <Footer />
