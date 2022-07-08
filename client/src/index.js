b2aed3586017db912de350747abae1f83a5f8e15
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-08 14:13:44 +0300

    connect application to store

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+import { Provider } from "react-redux";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+
+import { store } from "./store/index";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root"),
 );
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
