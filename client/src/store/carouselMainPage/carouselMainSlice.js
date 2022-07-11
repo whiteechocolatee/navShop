8da650fb4c7dcfcd84f421f937e6e8fad81d8f89
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-11 14:14:35 +0300

    refactor my code for better style

--- client/src/store/carouselMainPage/carouselMainSlice.js
+++ client/src/store/carouselMainPage/carouselMainSlice.js
@@ -1,4 +1,4 @@
-import carouselService from "../services/carouselService";
+import mainService from "../services/mainServices";
 import {
   createAsyncThunk,
   createSlice,
@@ -8,31 +8,7 @@ export const getNewItemsCarousel = createAsyncThunk(
   "getCarouselNew",
   async (_, thunkAPI) => {
     try {
-      return await carouselService.getNewItems();
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data);
-    }
-  },
-);
-
-export const getItemsByCategoryCarousel = createAsyncThunk(
-  "getCarouselByCategory",
-  async (category, thunkAPI) => {
-    try {
-      return await carouselService.getItemsByCategory(
-        category,
-      );
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data);
-    }
-  },
-);
-
-export const getDiscountItemsCarousel = createAsyncThunk(
-  "getDiscountCarousel",
-  async (_, thunkAPI) => {
-    try {
-      return await carouselService.getDiscountItems();
+      return await mainService.getItems();
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data);
     }
@@ -42,78 +18,28 @@ export const getDiscountItemsCarousel = createAsyncThunk(
 const carouselMainPage = createSlice({
   name: "carouselItems",
   initialState: {
-    items: {
-      newItems: [],
-      itemsByCategory: [],
-      discountItems: [],
-    },
-    isLoading: {
-      newItemsLoading: false,
-      ItemsByCategoryLoading: false,
-      discountItemsLoading: false,
-    },
+    items: [],
+    isLoading: false,
     isError: false,
     message: "",
   },
   extraReducers: (builder) => {
     builder
       .addCase(getNewItemsCarousel.pending, (state) => {
-        state.isLoading.newItemsLoading = true;
+        state.isLoading = true;
       })
       .addCase(
         getNewItemsCarousel.fulfilled,
         (state, action) => {
-          state.isLoading.newItemsLoading = false;
-          state.items.newItems = action.payload;
+          state.isLoading = false;
+          state.items = action.payload;
         },
       )
       .addCase(
         getNewItemsCarousel.rejected,
         (state, action) => {
-          state.isLoading.newItemsLoading = false;
-          state.items.newItems = null;
-          state.isError = action.error.message;
-        },
-      )
-      .addCase(
-        getItemsByCategoryCarousel.pending,
-        (state) => {
-          state.isLoading.ItemsByCategoryLoading = true;
-        },
-      )
-      .addCase(
-        getItemsByCategoryCarousel.fulfilled,
-        (state, action) => {
-          state.isLoading.ItemsByCategoryLoading = false;
-          state.items.itemsByCategory = action.payload;
-        },
-      )
-      .addCase(
-        getItemsByCategoryCarousel.rejected,
-        (state, action) => {
-          state.isLoading.ItemsByCategoryLoading = false;
-          state.items.itemsByCategory = null;
-          state.isError = action.error.message;
-        },
-      )
-      .addCase(
-        getDiscountItemsCarousel.pending,
-        (state) => {
-          state.isLoading.discountItemsLoading = true;
-        },
-      )
-      .addCase(
-        getDiscountItemsCarousel.fulfilled,
-        (state, action) => {
-          state.isLoading.discountItemsLoading = false;
-          state.items.discountItems = action.payload;
-        },
-      )
-      .addCase(
-        getDiscountItemsCarousel.rejected,
-        (state, action) => {
-          state.isLoading.discountItemsLoading = false;
-          state.items.discountItems = null;
+          state.isLoading = false;
+          state.items = null;
           state.isError = action.error.message;
         },
       );
