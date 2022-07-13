61ce16462ac3b2965d789bcb17db3e4f14e67c5e
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-13 19:08:02 +0300

    display cards by category at category page

--- client/src/pages/CategoryPage/CategoryPage.jsx
+++ client/src/pages/CategoryPage/CategoryPage.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 import { CategoriesNavigation } from "../../components/CategoriesNav/CategoriesNavigation";
 import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/Footer";
+import { Loader } from "../../components/Loader/Loader";
+
+import { getItemsByCategory } from "../../store/items/itemsSlice";
+import { ItemCarousel } from "../../components/ItemsByType/ItemCarousel";
 
 export const CategoryPage = () => {
   const { categoryName } = useParams();
+  const dispatch = useDispatch();
 
-  console.log(categoryName);
+  const { items, isLoading } = useSelector((state) => {
+    return state.itemsReducer;
+  });
+
+  console.log(items, isLoading);
+
+  useEffect(() => {
+    dispatch(getItemsByCategory(categoryName));
+  }, [categoryName, dispatch]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <React.Fragment>
       <Header />
       <CategoriesNavigation />
+      <ItemCarousel
+        itemsQuantity={16}
+        title={`Категория ${categoryName}`}
+        items={items}
+      />
+      <Footer />
     </React.Fragment>
   );
 };
