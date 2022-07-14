194b9ee44e1046e3412d6d7184df00add3d68aa7
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-14 14:21:40 +0300

    insert all item card to link which provide to single item page

--- client/src/components/ItemCard/Item.jsx
+++ client/src/components/ItemCard/Item.jsx
@@ -5,47 +5,52 @@ import { FaRegHeart } from "react-icons/fa";
 import { Button } from "../Button/Button";
 
 import { Image } from "cloudinary-react";
+import { Link } from "react-router-dom";
+import { paths } from "../../paths";
 
 export const Item = ({
   title = "",
   price = "",
   itemImg = "",
   discount = "",
+  id = "",
 }) => {
   return (
     <div className={styles.cardBody}>
-      <div className={styles.cardImageBlock}>
-        <div className={styles.addToFavorite}>
-          <div
-            className={`${
-              discount > 0 ? styles.sale : styles.saleNone
-            }`}>
-            {discount} %
+      <Link className={styles.itemLink} to={`${paths.itemPage}/${id}`}>
+        <div className={styles.cardImageBlock}>
+          <div className={styles.addToFavorite}>
+            <div
+              className={`${
+                discount > 0 ? styles.sale : styles.saleNone
+              }`}>
+              {discount} %
+            </div>
+            <FaRegHeart />
           </div>
-          <FaRegHeart />
+          <div className={styles.cardImg}>
+            <Image
+              cloudName='dmhqzwtnd'
+              publicId={itemImg}
+              alt={title}
+            />
+          </div>
         </div>
-        <div className={styles.cardImg}>
-          <Image
-            cloudName='dmhqzwtnd'
-            publicId={itemImg}
-            alt={title}
+        <div className={styles.description}>{title}</div>
+        <div className={styles.addToCart}>
+          <Button
+            className={styles.btnText}
+            containerClassName={styles.btnContainer}
+            children={`В корзину`}
           />
+          <span className={styles.price}>
+            {discount > 0
+              ? Math.ceil(price - (price / 100) * discount)
+              : price}{" "}
+            ₴
+          </span>
         </div>
-      </div>
-      <div className={styles.description}>{title}</div>
-      <div className={styles.addToCart}>
-        <Button
-          className={styles.btnText}
-          containerClassName={styles.btnContainer}
-          children={`В корзину`}
-        />
-        <span className={styles.price}>
-          {discount > 0
-            ? Math.ceil(price - (price / 100) * discount)
-            : price}{" "}
-          ₴
-        </span>
-      </div>
+      </Link>
     </div>
   );
 };
