ee5b2b8fe2c6216ec612fd30ca85d310350da5a1
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-14 14:49:37 +0300

    Create CategoryPage.jsx

---
+++ client/src/pages/Category/CategoryPage.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useSortItems } from "../../hooks/useSort";
+
+import { CategoriesNavigation } from "../../components/CategoriesNav/CategoriesNavigation";
+import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/Footer";
+import { Loader } from "../../components/Loader/Loader";
+import { ItemCarousel } from "../../components/ItemsByType/ItemCarousel";
+import { Button } from "../../components/Button/Button";
+import styles from "./category.module.css";
+
+import { getItemsByCategory } from "../../store/items/itemsSlice";
+
+export const CategoryPage = () => {
+  const { categoryName } = useParams();
+  const dispatch = useDispatch();
+
+  const { items, isLoading } = useSelector((state) => {
+    return state.itemsReducer;
+  });
+
+  const { desc, setDesc, sortedItems } = useSortItems(
+    items || [],
+  );
+
+  useEffect(() => {
+    dispatch(getItemsByCategory(categoryName));
+  }, [categoryName, dispatch]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <React.Fragment>
+      <Header />
+      <CategoriesNavigation />
+      <ItemCarousel
+        itemsQuantity={16}
+        // title={`Category ${categoryName}`}
+        items={sortedItems}>
+        <Button
+          containerClassName={styles.sortButton}
+          onClick={() => setDesc(!desc)}>
+          Сортировать по цене:{" "}
+          {`${desc ? "по убыванию" : "по возрастанию"}`}
+        </Button>
+      </ItemCarousel>
+      <Footer />
+    </React.Fragment>
+  );
+};
