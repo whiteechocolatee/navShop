910fc0fcce4aef49387772983e81230cf525e30b
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-14 14:23:24 +0300

    add new condition if items quantity bigger than all items which page have we just hide pagination buttons

--- client/src/components/ItemsByType/ItemCarousel.jsx
+++ client/src/components/ItemsByType/ItemCarousel.jsx
@@ -10,8 +10,9 @@ export const ItemCarousel = ({
   title = "",
   items = [],
   itemsQuantity,
+  children = "",
 }) => {
-  items = items.slice(0, 16);
+  items = items.slice(0, 12);
 
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(itemsQuantity);
@@ -31,8 +32,14 @@ export const ItemCarousel = ({
     <ContentWrapper>
       <div className={styles.cardContent}>
         <h1 className={styles.itemsTitle}>{title}</h1>
+        {children}
         <ItemsContainer currentItems={currentItems} />
-        <div className={styles.paginate}>
+        <div
+          className={
+            itemsQuantity >= items.length
+              ? styles.hide
+              : styles.paginate
+          }>
           <Pagination
             itemsPerPage={itemsPerPage}
             totalItems={items.length}
