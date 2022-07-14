7f8c2bc8c86f2088106c477495244f33c7539b82
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-14 14:50:50 +0300

    crate single item slice

---
+++ client/src/store/item/itemSlice.js
@@ -0,0 +1,42 @@
+import mainService from "../services/mainServices";
+import {
+  createSlice,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
+
+export const getItem = createAsyncThunk(
+  "getItemById",
+  async (id, thunkAPI) => {
+    try {
+      return await mainService.getItem(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const itemSlice = createSlice({
+  name: "item",
+  initialState: {
+    item: [],
+    isLoading: false,
+    isError: false,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getItem.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getItem.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.item = action.payload;
+      })
+      .addCase(getItem.rejected, (state, action) => {
+        state.isLoading = false;
+        state.item = null;
+        state.isError = action.payload.data;
+      });
+  },
+});
+
+export default itemSlice.reducer;
