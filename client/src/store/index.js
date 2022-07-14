7a78dde2d6940749aabe5804f4a1d64fd00010f1
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-14 14:50:31 +0300

    include new single item reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import itemsSlice from "./items/itemsSlice";
-import formsSlice from "./form/formsSlice";
+import formsSlice from "./forms/formsSlice";
+import itemSlice from "./item/itemSlice";
 
 export const store = configureStore({
   reducer: {
     itemsReducer: itemsSlice,
     formReducer: formsSlice,
+    singleItemReducer: itemSlice,
   },
 });
