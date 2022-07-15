b1f6306b8f6355c6c7ff47f6b38318340f5c28f1
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-15 18:54:24 +0300

    create middleware function and few conditions for checking token

---
+++ utils/authMiddleware.js
@@ -0,0 +1,39 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const protection = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET,
+      );
+
+      req.user = await User.findById(decoded.id).select(
+        "-password",
+      );
+
+      next();
+    } catch (error) {
+      res.status(401).json({
+        message:
+          "Не авторизирован, срок действия токена истек",
+      });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({
+      message: "Не авторизирован, токен не найден",
+    });
+  }
+};
+
+module.exports = protection;
