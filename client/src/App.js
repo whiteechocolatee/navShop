395d7f333965c564739de187bbbd91c0afe30dcc
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-17 14:48:54 +0300

    add exact property for urls

--- client/src/App.js
+++ client/src/App.js
@@ -12,19 +12,33 @@ import { Main } from "./pages/Main/Main";
 import { Error } from "./pages/Error/Error";
 import { CategoryPage } from "./pages/Category/CategoryPage";
 import { SingleItemPage } from "./pages/SingleItem/SingleItemPage";
+import { Registration } from "./pages/Registration/Registration";
+import { Login } from "./pages/Login/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={paths.main} element={<Main />} />
+        <Route exact path={paths.main} element={<Main />} />
         <Route
+          exact
           path={`${paths.category}/:categoryName`}
           element={<CategoryPage />}
         />
         <Route
+          exact
           path={`${paths.itemPage}/:id`}
           element={<SingleItemPage />}
+        />
+        <Route
+          exact
+          path={paths.signup}
+          element={<Registration />}
+        />
+        <Route
+          exact
+          path={paths.login}
+          element={<Login />}
         />
         <Route path='*' element={<Error />} />
       </Routes>
