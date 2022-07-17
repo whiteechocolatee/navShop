a889155581479b78188d558d2aae7dcdf2c863b4
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-17 18:30:39 +0300

    connect user slice to main store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import itemsSlice from "./items/itemsSlice";
 import formsSlice from "./forms/formsSlice";
 import itemSlice from "./item/itemSlice";
+import userSlice from "./users/userSlice";
 
 export const store = configureStore({
   reducer: {
     itemsReducer: itemsSlice,
     formReducer: formsSlice,
     singleItemReducer: itemSlice,
+    userReducer: userSlice,
   },
 });
