b2768eb8743fcf822f51fe747dd406a1d83f51b4
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-17 14:46:53 +0300

    create func before saving to db hashing password

--- models/userModel.js
+++ models/userModel.js
@@ -22,6 +22,8 @@ const userModel = mongoose.Schema({
   },
 });
 
+/* This is a method that is being added to the userModel. It is a function that takes in an entered
+password and compares it to the password that is stored in the database. */
 userModel.methods.matchPassword = async function (
   enteredPassword,
 ) {
@@ -30,5 +32,13 @@ userModel.methods.matchPassword = async function (
     this.password,
   );
 };
+
+userModel.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(7);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 module.exports = mongoose.model("User", userModel);
