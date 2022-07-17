d86122ad48ed67ca7ed6d2862e1ff37509b1e9e1
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-17 14:51:05 +0300

    change error page styles and include useNavigate hook for stepping back

--- client/src/pages/Error/Error.jsx
+++ client/src/pages/Error/Error.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { Button } from "../../components/Button/Button";
 import styles from "./error.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../../components/Button/Button";
 
 export const Error = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.errorBlock}>
       <div class={styles.main}>
         <div class={styles.head}>
           <h1>404</h1>
-          <h2>Страница не найдена</h2>
-          <Link className={styles.btnBack} to={"/"}>
-            {`>>> Вернуться на главную <<<`}
-          </Link>
+          <h2>
+            <Button
+              className={styles.btnBack}
+              onClick={() => navigate(-1)}>
+              Вернуться назад
+            </Button>
+          </h2>
         </div>
       </div>
     </div>
