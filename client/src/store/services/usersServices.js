6a8ebb6fb5112366fa2cfa7ad67d5eb40189c9bf
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-17 18:29:01 +0300

    create userSlice and services

---
+++ client/src/store/services/usersServices.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const userLogin = async ({ email, password }) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  let res = await axios.post(
+    "/api/users/login",
+    { email, password },
+    config,
+  );
+
+  localStorage.setItem(
+    "userInfo",
+    JSON.stringify(res.data),
+  );
+
+  return res.data;
+};
+
+const userServices = { userLogin };
+
+export default userServices;
