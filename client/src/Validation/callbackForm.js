9daabefb11da355018e62f88005e6c92456d8389
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-18 15:43:42 +0300

    insert new validation setting

--- client/src/Validation/callbackForm.js
+++ client/src/Validation/callbackForm.js
@@ -2,5 +2,10 @@ import * as yup from "yup";
 
 export const userSchema = yup.object().shape({
   name: yup.string().min(2).required(),
-  phone: yup.number().min(10).required(),
+  phone: yup
+    .string()
+    .required()
+    .matches(/^[0-9]+$/)
+    .min(9)
+    .max(12),
 });
