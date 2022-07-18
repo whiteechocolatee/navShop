bb59fc0467cf098aed60ac7ea698a5257443becf
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-18 17:07:46 +0300

    create registration page and add some logic , soon will add front end validation

--- client/src/pages/Registration/Registration.jsx
+++ client/src/pages/Registration/Registration.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import styles from "./registration.module.css";
 import { ContentWrapper } from "../../components/contentWrapper/ContentWrapper";
 import { Footer } from "../../components/Footer/Footer";
@@ -7,10 +9,35 @@ import { Button } from "../../components/Button/Button";
 import { Input } from "../../components/Input/Input";
 import { NavLink } from "react-router-dom";
 import { paths } from "../../paths";
+import { Loader } from "../../components/Loader/Loader";
+
+import { userRegister } from "../../store/users/userRegisterSlice";
 
 export const Registration = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { errors, isLoading } = useSelector(
+    (state) => state.userSignReducer,
+  );
+
+  const dispatch = useDispatch();
+
   const handleSubmit = () => {
-    console.log(1);
+    const data = {
+      name: name,
+      email: email,
+      password: password,
+    };
+
+    dispatch(userRegister(data)).then((res) => {
+      let response = res.type.split("/")[1] === "fulfilled";
+
+      if (response) {
+        window.location.replace(paths.account);
+      }
+    });
   };
 
   return (
@@ -19,32 +46,69 @@ export const Registration = () => {
       <ContentWrapper>
         <div className={styles.registrationPage}>
           <div className={styles.formBlock}>
-            <form className={styles.form}>
-              <h4>Впервые на сайте?</h4>
-              <Input
-                name='name'
-                className={styles.input}
-                type='text'
-                placeholder='Введите имя'
-              />
-              <Input
-                name='email'
-                className={styles.input}
-                type='text'
-                placeholder='Введите email'
-              />
-              <Input
-                name='password'
-                className={styles.input}
-                type='password'
-                placeholder='Введите пароль'
-              />
-              <Button
-                onClick={handleSubmit}
-                containerClassName={styles.btn}>
-                Зарегестрироваться
-              </Button>
-            </form>
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <form className={styles.form}>
+                {errors && errors.message ? (
+                  <div className={styles.errorBlock}>
+                    {errors.message}
+                  </div>
+                ) : (
+                  ""
+                )}
+                <h4>Впервые на сайте?</h4>
+                <Input
+                  name='name'
+                  className={styles.input}
+                  type='text'
+                  placeholder='Введите имя'
+                  onChange={(e) => setName(e.target.value)}
+                />
+                {errors && errors.name ? (
+                  <span className={styles.errorMessage}>
+                    {errors.name.message}
+                  </span>
+                ) : (
+                  ""
+                )}
+                <Input
+                  name='email'
+                  className={styles.input}
+                  type='text'
+                  placeholder='Введите email'
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {errors && errors.email ? (
+                  <span className={styles.errorMessage}>
+                    {errors.email.message}
+                  </span>
+                ) : (
+                  ""
+                )}
+                <Input
+                  name='password'
+                  className={styles.input}
+                  type='password'
+                  placeholder='Введите пароль'
+                  onChange={(e) =>
+                    setPassword(e.target.value)
+                  }
+                />
+                {errors && errors.password ? (
+                  <span className={styles.errorMessage}>
+                    {errors.password.message}
+                  </span>
+                ) : (
+                  ""
+                )}
+                <Button
+                  onClick={handleSubmit}
+                  containerClassName={styles.btn}>
+                  Зарегестрироваться
+                </Button>
+              </form>
+            )}
           </div>
           <div className={styles.redirect}>
             <NavLink to={paths.login}>
