6f8edc74b7020289bca0e025b7d7a3d43d3c2ea3
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-18 18:34:42 +0300

    add condition admin or user

--- client/src/pages/userAccount/UserAccount.jsx
+++ client/src/pages/userAccount/UserAccount.jsx
@@ -3,6 +3,12 @@ import { paths } from "../../paths";
 import { Header } from "../../components/Header/Header";
 
 export const UserAccount = () => {
+  const { isAdmin } = JSON.parse(
+    localStorage.getItem("userInfo"),
+  );
+
+  console.log(isAdmin);
+
   const handleLogout = () => {
     localStorage.removeItem("userInfo");
     window.location.replace(paths.main);
@@ -12,6 +18,11 @@ export const UserAccount = () => {
     <div>
       <Header />
       <div>Привет , новый польователь</div>
+      {isAdmin && (
+        <div>
+          <h1>Здравствуйте, Администратор!</h1>
+        </div>
+      )}
       <button onClick={handleLogout}>
         Выйти из аккаунта
       </button>
