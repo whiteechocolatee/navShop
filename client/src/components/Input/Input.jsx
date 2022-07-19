2cdc4de1bf59cea6c84a7fab6676ebe796d244f3
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-19 14:26:38 +0300

    edit input component delete parent block, insert props into input

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./input.module.css";
 
-export const Input = ({
-  className = "",
-  onChange = () => null,
-  type = "",
-  placeholder = "",
-  name = "",
-  error = "",
-}) => {
+export const Input = (props) => {
+  const {
+    className,
+    errorMessage,
+    onChange,
+    error,
+    ...inputProps
+  } = props;
+
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
-    <div className={styles.inputClassName}>
-      <div className={className}>
-        <input
-          type={type}
-          placeholder={placeholder}
-          name={name}
-          onChange={onChange}
-          className={`${styles.input}`}
-        />
-      </div>
+    <div className={`${styles.inputClassName}`}>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        className={`${styles.input} ${className}`}
+        onBlur={handleFocus}
+        focused={focused.toString()}
+      />
+      <span className={styles.validate}>
+        {errorMessage}
+      </span>
       {error && (
         <span className={styles.error}>{error}</span>
       )}
