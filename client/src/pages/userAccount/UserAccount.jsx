18d69d1b1fb2d2b726d19d690592c7c2c8995cc4
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-20 17:13:27 +0300

    create user account styles , create user panel for changing personal data

--- client/src/pages/userAccount/UserAccount.jsx
+++ client/src/pages/userAccount/UserAccount.jsx
@@ -1,31 +1,142 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import moment from "moment";
+import Message from "react-message-block";
+
+import styles from "./user.module.css";
 import { paths } from "../../paths";
 import { Header } from "../../components/Header/Header";
+import {
+  logout,
+  userProfile,
+} from "../../store/users/userAuthSlice";
+import { ContentWrapper } from "../../components/contentWrapper/ContentWrapper";
+import { Button } from "../../components/Button/Button";
+import { Footer } from "../../components/Footer/Footer";
+import { Input } from "../../components/Input/Input";
 
 export const UserAccount = () => {
-  const { isAdmin } = JSON.parse(
-    localStorage.getItem("userInfo"),
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, errors } = useSelector(
+    (state) => state.userAuthReducer,
   );
 
-  console.log(isAdmin);
+  const [values, setValues] = useState({
+    name: user.name,
+    email: user.email,
+    password: "",
+    confirmPassword: "",
+  });
+
+  useEffect(() => {
+    dispatch(userProfile());
+  }, [dispatch]);
 
   const handleLogout = () => {
-    localStorage.removeItem("userInfo");
-    window.location.replace(paths.main);
+    dispatch(logout());
+    window.localStorage.removeItem("token");
+    navigate(paths.main);
   };
+
+  const inputs = [
+    {
+      id: 1,
+      name: "name",
+      type: "text",
+      placeholder: "Изменить имя",
+      errorMessage: "Имя не должно содержать пробелов.",
+      pattern: "^[A-Za-z0-9]{3,10}$",
+      required: true,
+      error: errors && errors.name && errors.name.message,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Изменить почту",
+      errorMessage: "Укажите корректно почту!",
+      required: true,
+      error: errors && errors.email && errors.email.message,
+    },
+    {
+      id: 3,
+      name: "password",
+      type: "password",
+      placeholder: "Изменить пароль",
+      errorMessage:
+        "Пароль должен быть 8-20 символов и содержать как минимум - 1 букву, цифру и символ(!@#$%^&*)",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+      error:
+        errors &&
+        errors.password &&
+        errors.password.message,
+    },
+    {
+      id: 4,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Подтвердите пароль",
+      errorMessage: "Пароли не совпадают",
+      pattern: values.password,
+      required: true,
+    },
+  ];
+
+  const onChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(user);
 
   return (
     <div>
       <Header />
-      <div>Привет , новый польователь</div>
-      {isAdmin && (
-        <div>
-          <h1>Здравствуйте, Администратор!</h1>
+      <ContentWrapper>
+        <div className={styles.profile}>
+          {errors ? (
+            <Message text={errors} type='error' />
+          ) : (
+            ""
+          )}
+
+          <div className={styles.userInformation}>
+            <div className={styles.userName}>
+              <h3>{user.name}</h3>
+              <b>
+                Присоединился{" "}
+                {moment(user.createdAt).format("LL")}{" "}
+              </b>
+              <Button
+                onClick={handleLogout}
+                containerClassName={styles.btn}
+                children={"Выйти"}
+              />
+            </div>
+            <div className={styles.userData}>
+              {inputs.map((input) => (
+                <Input
+                  key={input.id}
+                  {...input}
+                  value={values[input.name]}
+                  onChange={onChange}
+                />
+              ))}
+              <Button
+                containerClassName={styles.btn}
+                children={"Изменить данные"}
+              />
+            </div>
+          </div>
         </div>
-      )}
-      <button onClick={handleLogout}>
-        Выйти из аккаунта
-      </button>
+      </ContentWrapper>
+      <Footer />
     </div>
   );
 };
