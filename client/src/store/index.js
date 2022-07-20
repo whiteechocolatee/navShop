c3bdb9a265637cd92f17d5abbfbcce71370dc648
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-20 17:14:42 +0300

    change reducers

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,16 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import itemsSlice from "./items/itemsSlice";
 import formsSlice from "./forms/formsSlice";
 import itemSlice from "./item/itemSlice";
-import userLoginSlice from "./users/userLoginSlice";
-import userRegisterSlice from "./users/userRegisterSlice";
+import userAuthSlice from "./users/userAuthSlice";
 
 export const store = configureStore({
   reducer: {
     itemsReducer: itemsSlice,
-    formReducer: formsSlice,
     singleItemReducer: itemSlice,
-    userLogReducer: userLoginSlice,
-    userSignReducer: userRegisterSlice,
-
+    formReducer: formsSlice,
+    userAuthReducer: userAuthSlice,
   },
 });
