3b53bc891ffde67ffee1424e3a0307fa1e9d4cbe
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-25 18:41:37 +0300

    include new actions to delivery slice

--- client/src/store/deliveryAddresses/deliverySlice.js
+++ client/src/store/deliveryAddresses/deliverySlice.js
@@ -4,11 +4,11 @@ import {
 } from "@reduxjs/toolkit";
 import deliveryService from "../services/deliveryService";
 
-export const getCities = createAsyncThunk(
-  "getCities",
+export const getRegions = createAsyncThunk(
+  "getRegions",
   async (_, thunkAPI) => {
     try {
-      return await deliveryService.getCities();
+      return await deliveryService.getRegions();
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data);
     }
@@ -18,23 +18,59 @@ export const getCities = createAsyncThunk(
 const deliverySlice = createSlice({
   name: "delivery",
   initialState: {
+    shippingData: [],
+    regions: [],
     cities: [],
+    departments: [],
     isLoading: false,
+  },
+  reducers: {
+    filteringByRegions: (state, action) => {
+      state.isLoading = true;
+      state.regions = state.shippingData.filter((item) => {
+        return item.SettlementAreaDescription.toLowerCase().includes(
+          action.payload.toLowerCase(),
+        );
+      });
+      state.isLoading = false;
+    },
+    filteringByCity: (state, action) => {
+      state.isLoading = true;
+      state.cities = state.regions.filter((item) => {
+        return item.SettlementDescription.toLowerCase().includes(
+          action.payload.toLowerCase(),
+        );
+      });
+      state.isLoading = false;
+    },
+    filteringDepartments: (state, action) => {
+      state.isLoading = true;
+      state.departments = state.cities.filter((item) => {
+        return item.Number.includes(action.payload);
+      });
+      state.isLoading = false;
+    },
   },
   extraReducers: (builder) => {
     builder
-      .addCase(getCities.pending, (state) => {
+      .addCase(getRegions.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(getCities.fulfilled, (state, action) => {
+      .addCase(getRegions.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.cities = action.payload;
+        state.shippingData = action.payload;
       })
-      .addCase(getCities.rejected, (state, action) => {
+      .addCase(getRegions.rejected, (state, action) => {
         state.isLoading = false;
-        state.cities = action.payload.data;
+        state.shippingData = action.payload.data;
       });
   },
 });
+
+export const {
+  filteringByRegions,
+  filteringByCity,
+  filteringDepartments,
+} = deliverySlice.actions;
 
 export default deliverySlice.reducer;
