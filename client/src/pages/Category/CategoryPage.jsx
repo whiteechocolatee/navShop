06ad3b4f7764f4a34f98c377cf63d3e5c13aa82d
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-26 19:51:35 +0300

    change sort button to sort select

--- client/src/pages/Category/CategoryPage.jsx
+++ client/src/pages/Category/CategoryPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useSortItems } from "../../hooks/useSort";
@@ -9,12 +9,12 @@ import { Footer } from "../../components/Footer/Footer";
 import { Loader } from "../../components/Loader/Loader";
 import { ItemCarousel } from "../../components/ItemsByType/ItemCarousel";
 import { Button } from "../../components/Button/Button";
-import styles from "./category.module.css";
-
-import { getItemsByCategory } from "../../store/items/itemsSlice";
 import { CallbackBlock } from "../../components/CallbackForm/CallbackBlock";
 import { ContentWrapper } from "../../components/contentWrapper/ContentWrapper";
 import { Filter } from "../../components/Filter/Filter";
+import styles from "./category.module.css";
+
+import { getItemsByCategory } from "../../store/items/itemsSlice";
 
 export const CategoryPage = () => {
   const { categoryName } = useParams();
@@ -24,9 +24,13 @@ export const CategoryPage = () => {
     return state.itemsReducer;
   });
 
-  const { desc, setDesc, sortedItems } = useSortItems(
+  const { setDesc, sortedItems } = useSortItems(
     items || [],
   );
+
+  const handleSort = (e) => {
+    setDesc(JSON.parse(e.target.value));
+  };
 
   useEffect(() => {
     dispatch(getItemsByCategory(categoryName));
@@ -47,15 +51,17 @@ export const CategoryPage = () => {
               itemsQuantity={16}
               title={`Category ${categoryName}`}
               items={sortedItems}>
-              <Button
-                containerClassName={styles.sortButton}
-                onClick={() => setDesc(!desc)}>
-                {`${
-                  desc
-                    ? "Сортировать по цене: по убыванию"
-                    : "Сортировать по цене: по возрастанию"
-                }`}
-              </Button>
+              <div className={styles.sortBlock}>
+                <label htmlFor='sort'>Відсортувати: </label>
+                <select name='sort' onChange={handleSort}>
+                  <option value={`true`}>
+                    найдорожче - найдешевше
+                  </option>
+                  <option value='false'>
+                    найдешевше - найдорожче
+                  </option>
+                </select>
+              </div>
             </ItemCarousel>
           </div>
         </ContentWrapper>
