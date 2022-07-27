4ac155c2111f1343f8e11e4ef72a5109280ca6c8
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-27 15:58:39 +0300

    saving cart to localStorage

--- client/src/store/cart/cartSlice.js
+++ client/src/store/cart/cartSlice.js
@@ -3,15 +3,25 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    itemsInCart: [],
+    itemsInCart: localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [],
   },
   reducers: {
     setItemInCart: (state, action) => {
       state.itemsInCart.push(action.payload);
+      localStorage.setItem(
+        "cart",
+        JSON.stringify(state.itemsInCart),
+      );
     },
     removeItemFromCart: (state, action) => {
       state.itemsInCart = state.itemsInCart.filter(
         (item) => item._id !== action.payload,
+      );
+      localStorage.setItem(
+        "cart",
+        JSON.stringify(state.itemsInCart),
       );
     },
   },
