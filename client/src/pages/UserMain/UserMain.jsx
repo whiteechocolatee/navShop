0397e4afb12cd2b16744c41d360207100e7e26ab
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-28 19:12:38 +0300

    delete all actions

--- client/src/pages/UserMain/UserMain.jsx
+++ client/src/pages/UserMain/UserMain.jsx
@@ -1,53 +1,27 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
-import { paths } from "../../paths";
+import { useDispatch } from "react-redux";
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
 import { UpdateUserInfo } from "../../components/UpdateForm/UpdateUserInfo";
 import { UserAccount } from "../../components/UserAccount/UserAccount";
 
-import {
-  logout,
-  userProfile,
-} from "../../store/users/userAuthSlice";
+import { userProfile } from "../../store/users/userAuthSlice";
 
 import { ToastContainer } from "react-toastify";
 
 export const UserMain = () => {
   window.scroll(0, 0);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
-
-  const { user, errors } = useSelector(
-    (state) => state.userAuthReducer,
-  );
-
-  const isAdmin = user.isAdmin;
 
   useEffect(() => {
     dispatch(userProfile());
   }, [dispatch]);
 
-  const handleLogout = () => {
-    dispatch(logout());
-    window.localStorage.removeItem("token");
-    navigate(paths.main);
-  };
-
   return (
     <>
-      <Header
-        handleLogout={handleLogout}
-        isAuth={user ? true : false}
-      />
+      <Header />
       <ToastContainer />
-      <UserAccount
-        errors={errors}
-        user={user}
-        children={<UpdateUserInfo />}
-      />
+      <UserAccount children={<UpdateUserInfo />} />
       <Footer />
     </>
   );
