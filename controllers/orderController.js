321b35844e4208971d26c1d92384121f29c65c3c
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-28 16:33:02 +0300

    create and include get order by user id to order routes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,9 +1,39 @@
 const Order = require("../models/orderModel");
 
+/**
+ * It finds all orders that have the same user id as the user that's logged in and sorts them by the
+ * most recent order
+ * @param req - The request object.
+ * @param res - The response object.
+ */
+const userOrders = async (req, res) => {
+  try {
+    const order = await Order.find({
+      user: req.user._id,
+    }).sort({ _id: -1 });
+
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json({
+      message: "Помилка сервера, зверніться пізніше.",
+    });
+  }
+};
+
+/**
+ * It creates an order
+ * @param req - The request object.
+ * @param res - The response object.
+ * @returns The order is being returned.
+ */
 const createOrder = async (req, res) => {
   try {
-    const { orderItems, shippingAddress, totalPrice } =
-      req.body;
+    const {
+      orderItems,
+      shippingAddress,
+      totalPrice,
+      customerData,
+    } = req.body;
     const user = req.user._id;
 
     if (orderItems && orderItems.length === 0) {
@@ -15,6 +45,7 @@ const createOrder = async (req, res) => {
         orderItems,
         shippingAddress,
         totalPrice,
+        customerData,
         user,
       });
 
@@ -29,4 +60,4 @@ const createOrder = async (req, res) => {
   }
 };
 
-module.exports = { createOrder };
+module.exports = { createOrder, userOrders };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const {
   createOrder,
+  userOrders,
 } = require("../controllers/orderController");
 const protection = require("../utils/authMiddleware");
 
 const router = express.Router();
 
-router.post("/", protection, createOrder);
+router
+  .post("/", protection, createOrder)
+  .get("/", protection, userOrders);
 
 module.exports = router;
