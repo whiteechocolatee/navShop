36795bf6545ca0369e55b8cf22acdf701aa3984d
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-28 19:13:20 +0300

    delet props

--- client/src/components/UserAccount/UserAccount.jsx
+++ client/src/components/UserAccount/UserAccount.jsx
@@ -3,8 +3,13 @@ import styles from "./account.module.css";
 import { ContentWrapper } from "../contentWrapper/ContentWrapper";
 import { UserInformation } from "../UserInformation/UserInformation";
 import { Message } from "react-message-block";
+import { useSelector } from "react-redux";
 
-export const UserAccount = ({ errors, children, user }) => {
+export const UserAccount = ({ children }) => {
+  const { errors } = useSelector(
+    (state) => state.userAuthReducer,
+  );
+
   return (
     <ContentWrapper>
       <div className={styles.profile}>
@@ -12,7 +17,7 @@ export const UserAccount = ({ errors, children, user }) => {
           <Message text={errors} type='error' />
         ) : (
           <div className={styles.userInformation}>
-            <UserInformation {...user} />
+            <UserInformation />
             <div className={styles.userData}>
               {children}
             </div>
