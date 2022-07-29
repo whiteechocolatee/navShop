be3bafc1c8a2a81a67ab357f86d841ac77335c21
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-29 16:55:55 +0300

    create design of cart where included orderCheck and orderCards

--- client/src/components/OrderCart/OrderCart.jsx
+++ client/src/components/OrderCart/OrderCart.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { OrderCard } from "../OrderCard/OrderCard";
 import { Link } from "react-router-dom";
 import { paths } from "../../paths";
+import { OrderCheck } from "../OrderCheck/OrderCheck";
 
 export const OrderCart = () => {
   const cart = useSelector((state) => {
@@ -16,23 +17,41 @@ export const OrderCart = () => {
 
   return (
     <div className={styles.order}>
+      <div className={styles.navigation}>
+        <Link className={styles.link} to={paths.main}>
+          Головна
+        </Link>
+        <hr className={styles.border} />
+        <Link className={styles.link} to={paths.order}>
+          Кошик
+        </Link>
+      </div>
+      <div>
+        <h1 className={styles.cartTitle}>Кошик</h1>
+      </div>
       {cart.length > 0 ? (
-        <div>
-          <h1>Сумма заказа : {total} uah</h1>
-          {cart.map((item, index) => (
-            <OrderCard
-              key={index}
-              title={item.title}
-              itemImage={item.itemImage}
-              price={item.price}
-            />
-          ))}
-          <Link to={`${paths.order}/${paths.delivery}`}>
-            Перейти к оформлению заказа
-          </Link>
+        <div className={styles.orderInfo}>
+          <div className={styles.orderCards}>
+            <h3 className={styles.orderTitle}>
+              Ваше замовлення
+            </h3>
+            <hr className={styles.orderBorder} />
+            {cart.map((item) => (
+              <OrderCard
+                key={item._id}
+                id={item._id}
+                title={item.title}
+                itemImage={item.itemImage}
+                price={item.price}
+              />
+            ))}
+          </div>
+          <OrderCheck total={total} />
         </div>
       ) : (
-        <h1>Упс.... Корзина пустая</h1>
+        <h1 className={styles.emptyCart}>
+          Ваш кошик пустий, додайте щось :)
+        </h1>
       )}
     </div>
   );
