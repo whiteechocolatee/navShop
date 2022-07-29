eab96b7dde3bca3077696f322c56bc6eb1862416
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-29 16:52:18 +0300

    change design of order card and include three functions delete from cart / add one / remove one

--- client/src/components/OrderCard/OrderCard.jsx
+++ client/src/components/OrderCard/OrderCard.jsx
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./orderCard.module.css";
-import { ImageComponent } from "../Image/Image";
 
-export const OrderCard = ({ itemImage, title, price }) => {
+import { useDispatch } from "react-redux";
+
+import { ImageComponent } from "../Image/Image";
+import { BiTrash, BiPlus, BiMinus } from "react-icons/bi";
+import { removeItemFromCart } from "../../store/cart/cartSlice";
+
+export const OrderCard = ({
+  itemImage,
+  title,
+  price,
+  id,
+}) => {
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
+
+  const handleDelete = (e) => {
+    e.stopPropagation();
+    dispatch(removeItemFromCart(id));
+  };
+
+  const addOne = (e) => {
+    e.stopPropagation();
+    setQty(qty + 1);
+  };
+  const removeOne = (e) => {
+    e.stopPropagation();
+    setQty(qty - 1);
+    if (qty < 2) {
+      dispatch(removeItemFromCart(id));
+    }
+  };
+
   return (
     <div className={styles.orderCard}>
       <div className={styles.imageBlock}>
         <ImageComponent publicId={itemImage} alt={title} />
       </div>
       <div className={styles.title}>
-        <h3>{title}</h3>
+        <h6 className={styles.titles}>{title}</h6>
+      </div>
+      <div className={styles.qty}>
+        <div className={styles.qtyBtns} onClick={removeOne}>
+          <BiMinus />
+        </div>
+        <div>{qty}</div>
+        <div className={styles.qtyBtns} onClick={addOne}>
+          <BiPlus />
+        </div>
+      </div>
+      <div className={styles.pricePerItem}>
+        <h6 className={styles.titles}>{price}</h6>
       </div>
       <div className={styles.price}>
-        <h2>{price}</h2>
+        <h6 className={styles.titles}>
+          <b>{price * qty}</b>
+        </h6>
+      </div>
+      <div onClick={handleDelete} className={styles.remove}>
+        <BiTrash />
       </div>
     </div>
   );
