efb75408b69e2b88c92b8153648a029c35dcaf2f
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-29 13:10:39 +0300

    get loader and orders from state

--- client/src/components/Orders/Orders.jsx
+++ client/src/components/Orders/Orders.jsx
@@ -6,36 +6,41 @@ import { useEffect } from "react";
 import { getUserOrders } from "../../store/order/orderSlice";
 import { AiOutlineEye } from "react-icons/ai";
 import moment from "moment";
+import { Loader } from "../Loader/Loader";
 
 export const Orders = () => {
   const dispatch = useDispatch();
 
-  const order = useSelector(
-    (state) => state.orderReducer.order,
+  const { orders, isLoading } = useSelector(
+    (state) => state.orderReducer,
   );
-
-  console.log(order);
 
   useEffect(() => {
     dispatch(getUserOrders());
   }, [dispatch]);
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <ContentWrapper>
       <h1>Ваші замовлення</h1>
       <div className={styles.orders}>
-        {order.map((item) => (
-          <div className={styles.order}>
-            <span>{item._id}</span>
-            <span>
-              {moment(item.createdAt).format("LL")}
-            </span>
-            <span>{item.totalPrice}</span>
-            <span>
-              <AiOutlineEye />
-            </span>
-          </div>
-        ))}
+        {orders.length > 0
+          ? orders.map((item) => (
+              <div key={item._id} className={styles.order}>
+                <span>{item._id}</span>
+                <span>
+                  {moment(item.createdAt).format("LL")}
+                </span>
+                <span>{item.totalPrice}</span>
+                <span>
+                  <AiOutlineEye />
+                </span>
+              </div>
+            ))
+          : <h4>У вас немає замовлень.</h4>}
       </div>
     </ContentWrapper>
   );
