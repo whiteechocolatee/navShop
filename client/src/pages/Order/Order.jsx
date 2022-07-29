53e54d6c057a397fdd17167e7fff3d390176d6fc
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-29 16:56:14 +0300

    put categories navigation to cart page

--- client/src/pages/Order/Order.jsx
+++ client/src/pages/Order/Order.jsx
@@ -4,6 +4,7 @@ import { ContentWrapper } from "../../components/contentWrapper/ContentWrapper";
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
 import { OrderCart } from "../../components/OrderCart/OrderCart";
+import { CategoriesNavigation } from "../../components/CategoriesNav/CategoriesNavigation";
 
 export const Order = () => {
   window.scroll(0, 0);
@@ -11,6 +12,7 @@ export const Order = () => {
   return (
     <React.Fragment>
       <Header />
+      <CategoriesNavigation />
       <ContentWrapper className={styles.orderPage}>
         <OrderCart />
       </ContentWrapper>
