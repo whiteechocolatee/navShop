0949b3ffc8868a2cbb5bb1914ed29c4cfea7c61d
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-30 18:35:01 +0300

    include categories nav to delivery page

--- client/src/pages/Delivery/Delivery.jsx
+++ client/src/pages/Delivery/Delivery.jsx
@@ -5,11 +5,13 @@ import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
 import { ContentWrapper } from "../../components/contentWrapper/ContentWrapper";
 import { OrderInfo } from "../../components/OrderInfo/OrderInfo";
+import { CategoriesNavigation } from "../../components/CategoriesNav/CategoriesNavigation";
 
 export const Delivery = () => {
   return (
     <React.Fragment>
       <Header />
+      <CategoriesNavigation />
       <ContentWrapper className={styles.wrapper}>
         <OrderInfo />
       </ContentWrapper>
