aa98c5d5349625179918bafbead5724252ea2051
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-31 18:04:02 +0300

    create new services for requesting data from nova poshta api

--- client/src/store/services/deliveryService.js
+++ client/src/store/services/deliveryService.js
@@ -1,16 +1,30 @@
 import axios from "axios";
 
-const getRegions = async () => {
+const api = "https://api.novaposhta.ua/v2.0/json/";
+const apiKey = "d6a9f94157b12fe4993a41f31d276bef";
+
+const getAreas = async () => {
   let config = {
-    apiKey: "d6a9f94157b12fe4993a41f31d276bef",
+    apiKey: apiKey,
+    modelName: "Address",
+    calledMethod: "getAreas",
+  };
+
+  let response = await axios.post(api, config);
+
+  const { data } = response.data;
+
+  return data;
+};
+
+const getAllDepartments = async () => {
+  const config = {
+    apiKey: apiKey,
     modelName: "Address",
     calledMethod: "getWarehouses",
   };
 
-  let response = await axios.post(
-    "https://api.novaposhta.ua/v2.0/json/",
-    config,
-  );
+  let response = await axios.post(api, config);
 
   const { data } = response.data;
 
@@ -18,7 +32,8 @@ const getRegions = async () => {
 };
 
 const deliveryService = {
-  getRegions,
+  getAreas,
+  getAllDepartments,
 };
 
 export default deliveryService;
