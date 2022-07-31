4a0e8423c3d3722586f39b60a9167348fef854bc
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-31 18:03:24 +0300

    create ability to choosing nova poshta department

--- client/src/components/NovaPoshta/NovaPoshta.jsx
+++ client/src/components/NovaPoshta/NovaPoshta.jsx
@@ -1,5 +1,81 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAreas,
+  getAllDepartments,
+  getCitiesByArea,
+  getDepartmentsByCity,
+} from "../../store/deliveryAddresses/deliverySlice";
+import styles from "./poshta.module.css";
+import { Loader } from '../Loader/Loader';
 
 export const NovaPoshta = () => {
-  return <div>NovaPoshta</div>;
+  const dispatch = useDispatch();
+
+  const { isLoading, areas, cities, departmentByCity } =
+    useSelector((state) => state.deliveryReducer);
+
+  useEffect(() => {
+    dispatch(getAreas());
+    dispatch(getAllDepartments());
+  }, [dispatch]);
+
+  const handleCities = (e) => {
+    dispatch(getCitiesByArea(e.target.value));
+  };
+
+  const handleDepartments = (e) => {
+    dispatch(getDepartmentsByCity(e.target.value));
+  };
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <div className={styles.selects}>
+      <div>
+        <select
+          className={styles.select}
+          onChange={handleCities}>
+          <option value='' disabled selected>
+            Виберіть область
+          </option>
+          {areas.map((area, index) => (
+            <option key={index} value={area.Description}>
+              {area.Description}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <select
+          className={styles.select}
+          onChange={handleDepartments}>
+          <option value='' disabled selected>
+            Виберіть населенний пункт
+          </option>
+          {cities.map((city, index) => (
+            <option key={index} value={city}>
+              {`${city}`}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <select className={styles.select}>
+          <option value='' disabled selected>
+            Виберіть відділення
+          </option>
+          {departmentByCity.map((department, index) => (
+            <option
+              key={index}
+              value={department.Description}>
+              {`${department.Description}`}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 };
