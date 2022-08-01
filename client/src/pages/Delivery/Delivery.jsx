3ff224661a1ce3114190f96086f5c4bc2e301c57
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-01 16:56:45 +0300

    get data from all fields and send to server

--- client/src/pages/Delivery/Delivery.jsx
+++ client/src/pages/Delivery/Delivery.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -16,6 +16,7 @@ import { Input } from "../../components/Input/Input";
 import { paths } from "../../paths";
 import { Loader } from "../../components/Loader/Loader";
 
+import { createOrder } from "../../store/order/orderSlice";
 import {
   getAreas,
   getAllDepartments,
@@ -23,6 +24,27 @@ import {
 
 export const Delivery = () => {
   const dispatch = useDispatch();
+
+  const [values, setValues] = useState({
+    name: "",
+    surname: "",
+    phone: "",
+    email: "",
+    shippingMethod: "localPickup",
+    area: "",
+    city: "",
+    index: "",
+    department: "",
+    paymentMethod: "cardPayment",
+    commentary: "",
+  });
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const cart = useSelector((state) => {
     return state.cartReducer.itemsInCart;
@@ -44,6 +66,57 @@ export const Delivery = () => {
   if (isLoading) {
     return <Loader />;
   }
+
+  const orderCart = [];
+
+  cart.map((item) => {
+    return orderCart.push({
+      name: item.title,
+      image: item.itemImage,
+      price: item.price,
+      count: item.count,
+      totalPrice: item.totalPrice,
+      product: item._id,
+    });
+  });
+
+  const order = {
+    orderItems: orderCart,
+    shippingAddress: {
+      shippingMethod: values.shippingMethod,
+      region:
+        values.shippingMethod === "localPickup"
+          ? "Харківська"
+          : values.area,
+      city:
+        values.shippingMethod === "localPickup"
+          ? "Харків"
+          : values.city,
+      department:
+        values.shippingMethod === "localPickup"
+          ? "ТРК 'Клас' (Проспект Тракторобудівників, 128в) , другий поверх."
+          : values.department,
+      index:
+        values.shippingMethod === "localPickup"
+          ? "61000"
+          : values.index,
+    },
+    customerData: {
+      name: values.name,
+      email: values.email,
+      surname: values.surname,
+      phone: values.phone,
+    },
+    totalPrice: total,
+    paymentMethod: values.paymentMethod,
+    commentary: values.commentary,
+  };
+
+  console.log(order);
+
+  const handleOrder = () => {
+    dispatch(createOrder(order));
+  };
 
   return (
     <React.Fragment>
@@ -71,27 +144,42 @@ export const Delivery = () => {
           </div>
           <div className={styles.orderPayment}>
             <div className={styles.forms}>
-              <CustomerForm />
-              <DeliveryMethod />
-              <Payment />
+              <CustomerForm
+                values={values}
+                handleChange={handleChange}
+              />
+              <DeliveryMethod
+                values={values}
+                handleChange={handleChange}
+              />
+              <Payment
+                values={values}
+                handleChange={handleChange}
+              />
               <div className={styles.comment}>
                 <h5 className={styles.commentTitle}>
                   Можливо вам є що додати до вашого
                   замовлення
                 </h5>
-                <Input className={styles.input} />
+                <Input
+                  name='commentary'
+                  onChange={handleChange}
+                  className={styles.input}
+                />
               </div>
             </div>
             <OrderCheckDelivery total={total} cart={cart} />
           </div>
         </div>
         <Button
+          onClick={handleOrder}
           children={
-            <Link
-              className={styles.btnLink}
-              to={paths.order}>
-              Перейти до оплати
-            </Link>
+            // <Link
+            //   className={styles.btnLink}
+            //   to={paths.order}>
+            //   Перейти до оплати
+            // </Link>
+            "Замовити!"
           }
           containerClassName={styles.btn}
         />
