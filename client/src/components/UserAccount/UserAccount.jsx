a5a5ea45eba05845d2a81af4803824ba6e55adc1
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-02 16:12:07 +0300

    change user account layout

--- client/src/components/UserAccount/UserAccount.jsx
+++ client/src/components/UserAccount/UserAccount.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import styles from "./account.module.css";
 import { ContentWrapper } from "../contentWrapper/ContentWrapper";
-import { UserInformation } from "../UserInformation/UserInformation";
 import { Message } from "react-message-block";
 import { useSelector } from "react-redux";
+import { paths } from "../../paths";
+import { Link, NavLink } from "react-router-dom";
 
 export const UserAccount = ({ children }) => {
   const { errors } = useSelector(
     (state) => state.userAuthReducer,
   );
+
+  const handleLogout = () => {};
 
   return (
     <ContentWrapper>
@@ -16,10 +19,60 @@ export const UserAccount = ({ children }) => {
         {errors ? (
           <Message text={errors} type='error' />
         ) : (
-          <div className={styles.userInformation}>
-            <UserInformation />
-            <div className={styles.userData}>
-              {children}
+          <div>
+            <div className={styles.navigation}>
+              <Link className={styles.link} to={paths.main}>
+                Головна
+              </Link>
+              <hr className={styles.border} />
+              <Link
+                className={styles.link}
+                to={paths.order}>
+                Профіль
+              </Link>
+            </div>
+            <h1 className={styles.title}>Мій профіль</h1>
+            <div className={styles.userNavigation}>
+              <nav className={styles.navigationBtns}>
+                <NavLink
+                  className={styles.links}
+                  style={({ isActive }) => ({
+                    color: isActive ? "#ABAFE9" : "#000",
+                  })}
+                  to={paths.account}>
+                  Мої дані
+                </NavLink>
+                <NavLink
+                  className={styles.links}
+                  style={({ isActive }) => ({
+                    color: isActive ? "#ABAFE9" : "#000",
+                  })}
+                  to={paths.userOrders}>
+                  Мої замовлення
+                </NavLink>
+                <NavLink
+                  className={styles.links}
+                  style={({ isActive }) => ({
+                    color: isActive ? "#ABAFE9" : "#000",
+                  })}
+                  to={`${paths.liked}`}>
+                  Мені сподобалось
+                </NavLink>
+                <div
+                  onClick={handleLogout}
+                  className={styles.logout}>
+                  вийти
+                  <div className={styles.arrow}>
+                    <div className={styles.arrowBody}></div>
+                    <div className={styles.arrowEnd}></div>
+                  </div>
+                </div>
+              </nav>
+            </div>
+            <div className={styles.userInformation}>
+              <div className={styles.userData}>
+                {children}
+              </div>
             </div>
           </div>
         )}
