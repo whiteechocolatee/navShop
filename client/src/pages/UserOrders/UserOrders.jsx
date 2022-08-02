e238fa42f31ee03fbb0c07bec025fbe3c3f7e0be
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-02 18:33:12 +0300

    create user orders page

--- client/src/pages/UserOrders/UserOrders.jsx
+++ client/src/pages/UserOrders/UserOrders.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
 import { Orders } from "../../components/Orders/Orders";
 import { UserAccount } from "../../components/UserAccount/UserAccount";
 import { CategoriesNavigation } from "../../components/CategoriesNav/CategoriesNavigation";
+import { ContentWrapper } from "../../components/contentWrapper/ContentWrapper";
 
 export const UserOrders = () => {
   return (
     <>
       <Header />
       <CategoriesNavigation />
-      <UserAccount children={<Orders />} />
+      <ContentWrapper>
+        <UserAccount />
+        <Orders />
+      </ContentWrapper>
       <Footer />
     </>
   );
