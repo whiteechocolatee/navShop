fc7cfabe08306bbfd18dbbb04e58cb33b1b45e6b
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-02 16:10:35 +0300

    create personal data block where user can add addresses to account and choosing like main

---
+++ client/src/components/PersonalData/PersonalData.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./personal.module.css";
+import { useSelector } from "react-redux";
+import { Button } from "../Button/Button";
+import { Link } from "react-router-dom";
+import { paths } from "../../paths";
+
+export const PersonalData = () => {
+  const { user } = useSelector(
+    (state) => state.userAuthReducer,
+  );
+
+  const handleChange = () => {
+    console.log(1);
+  };
+
+  return (
+    <div className={styles.personalBlock}>
+      <div className={styles.personalData}>
+        <span>{user.name}</span>
+        <span>{user.email}</span>
+        <span>{user.phoneNumber}</span>
+        <Button
+          onClick={handleChange}
+          containerClassName={styles.btn}
+          children={
+            <Link
+              className={styles.link}
+              to={`${paths.account}${paths.changeUserData}`}>
+              Змінити
+            </Link>
+          }
+        />
+      </div>
+      {/* map user addresses   */}
+    </div>
+  );
+};
