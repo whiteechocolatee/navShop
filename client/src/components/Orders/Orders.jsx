8bfee10ad4a6ef232f4f581f992be176728ee6a8
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-02 18:33:42 +0300

    showing orders to user

--- client/src/components/Orders/Orders.jsx
+++ client/src/components/Orders/Orders.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./userOrders.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { ContentWrapper } from "../contentWrapper/ContentWrapper";
 import { useEffect } from "react";
 import { getUserOrders } from "../../store/order/orderSlice";
-import { AiOutlineEye } from "react-icons/ai";
 import moment from "moment";
+import "moment/locale/uk";
 import { Loader } from "../Loader/Loader";
+import { UserOrderCard } from "../UserOrdersCard/UserOrderCard";
+import { ContentWrapper } from "../contentWrapper/ContentWrapper";
 
 export const Orders = () => {
   const dispatch = useDispatch();
@@ -23,24 +24,81 @@ export const Orders = () => {
     return <Loader />;
   }
 
+  console.log(orders);
+
   return (
-    <ContentWrapper>
-      <h1>Ваші замовлення</h1>
-      <div className={styles.orders}>
-        {orders.length > 0
-          ? orders.map((item) => (
-              <div key={item._id} className={styles.order}>
-                <span>{item._id}</span>
-                <span>
-                  {moment(item.createdAt).format("LL")}
-                </span>
-                <span>{item.totalPrice}</span>
-                <span>
-                  <AiOutlineEye />
-                </span>
+    <ContentWrapper className={styles.wrapper}>
+      <div>
+        {orders.map((order) => {
+          return (
+            <div key={order._id} className={styles.orders}>
+              <div className={styles.orderInfo}>
+                <div className={styles.orderDetails}>
+                  <span>
+                    {order._id} від{" "}
+                    {moment(order.createdAt).format("L")}
+                  </span>
+                  <span className={styles.isDelivery}>
+                    {order.isDelivered}
+                  </span>
+                </div>
+                <div className={styles.orderItems}>
+                  {order.orderItems.map((item) => (
+                    <UserOrderCard
+                      key={item._id}
+                      id={item._id}
+                      title={item.name}
+                      itemImage={item.image}
+                      price={item.price}
+                      count={item.count}
+                      totalPrice={item.totalPrice}
+                    />
+                  ))}
+                </div>
               </div>
-            ))
-          : <h4>У вас немає замовлень.</h4>}
+              <div className={styles.deliveryDetails}>
+                <div className={styles.payment}>
+                  <p className={styles.paymentTitle}>
+                    <b>Оплата</b>
+                  </p>
+                  <span>
+                    Сумма товарів: {order.totalPrice} ₴
+                  </span>
+                  <span>
+                    Доставка:{" "}
+                    {order.shippingAddress
+                      .shippingMethod === "localPickup"
+                      ? "Безкоштовно"
+                      : "за тарифами пошти"}
+                  </span>
+                  <span>
+                    До оплати:{" "}
+                    {`${order.totalPrice} ₴ + доставка`}
+                  </span>
+                  <span>
+                    Спосіб оплати: {order.paymentMethod}
+                  </span>
+                  <span>Оплачено: - </span>
+                </div>
+                <div className={styles.delivery}>
+                  <p className={styles.deliveryTitle}>
+                    <b>Доставка</b>
+                  </p>
+                  <span>
+                    Спосіб доставки:{" "}
+                    {order.shippingAddress.shippingMethod}
+                  </span>
+                  <span>
+                    Адресса: {order.shippingAddress.region}{" "}
+                    область ,{order.shippingAddress.city},{" "}
+                    {order.shippingAddress.department},{" "}
+                    {order.shippingAddress?.index}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </ContentWrapper>
   );
