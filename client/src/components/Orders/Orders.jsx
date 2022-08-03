e20435a13484e119fff16283cacf1870a8786073
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-03 17:01:34 +0300

    include orders cards and filter orders array completed/in progress

--- client/src/components/Orders/Orders.jsx
+++ client/src/components/Orders/Orders.jsx
@@ -3,10 +3,9 @@ import styles from "./userOrders.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getUserOrders } from "../../store/order/orderSlice";
-import moment from "moment";
-import "moment/locale/uk";
 import { Loader } from "../Loader/Loader";
-import { UserOrderCard } from "../UserOrdersCard/UserOrderCard";
+import { OrdersInProgress } from "../OrdersInProgress/OrdersInProgress";
+import { CompletedOrder } from "../CompletedOrders/CompletedOrder";
 
 export const Orders = () => {
   const dispatch = useDispatch();
@@ -23,82 +22,32 @@ export const Orders = () => {
     return <Loader />;
   }
 
+  const ordersInProgress = orders?.filter((order) => {
+    return order.isDelivered !== "Виконано";
+  });
+
+  const completedOrders = orders?.filter((order) => {
+    return order.isDelivered === "Виконано";
+  });
+
+  console.log("in progress >>>", ordersInProgress);
+  console.log("completed >>>", completedOrders);
+
   return (
     <div className={styles.orderContainer}>
       {orders.length > 0 ? (
-        orders.map((order) => {
-          return (
-            <div key={order._id} className={styles.orders}>
-              <div className={styles.orderInfo}>
-                <div className={styles.orderDetails}>
-                  <span>
-                    {order._id} від{" "}
-                    {moment(order.createdAt).format("L")}
-                  </span>
-                  <span className={styles.isDelivery}>
-                    {order.isDelivered}
-                  </span>
-                </div>
-                <div className={styles.orderItems}>
-                  {order.orderItems.map((item) => (
-                    <UserOrderCard
-                      key={item._id}
-                      id={item._id}
-                      title={item.name}
-                      itemImage={item.image}
-                      price={item.price}
-                      count={item.count}
-                      totalPrice={item.totalPrice}
-                    />
-                  ))}
-                </div>
-              </div>
-              <div className={styles.deliveryDetails}>
-                <div className={styles.payment}>
-                  <p className={styles.paymentTitle}>
-                    <b>Оплата</b>
-                  </p>
-                  <span>
-                    Сумма товарів: {order.totalPrice} ₴
-                  </span>
-                  <span>
-                    Доставка:{" "}
-                    {order.shippingAddress
-                      .shippingMethod === "localPickup"
-                      ? "Безкоштовно"
-                      : "за тарифами пошти"}
-                  </span>
-                  <span>
-                    До cплати: {order.totalPrice} ₴
-                    {order.shippingAddress
-                      .shippingMethod === "localPickup"
-                      ? ""
-                      : "+ за тарифами пошти"}
-                  </span>
-                  <span>
-                    Спосіб оплати: {order.paymentMethod}
-                  </span>
-                  <span>Cплачено: - </span>
-                </div>
-                <div className={styles.delivery}>
-                  <p className={styles.deliveryTitle}>
-                    <b>Доставка</b>
-                  </p>
-                  <span>
-                    Спосіб доставки:{" "}
-                    {order.shippingAddress.shippingMethod}
-                  </span>
-                  <span>
-                    Адресса: {order.shippingAddress.region}{" "}
-                    область ,{order.shippingAddress.city},{" "}
-                    {order.shippingAddress.department},{" "}
-                    {order.shippingAddress?.index}
-                  </span>
-                </div>
-              </div>
-            </div>
-          );
-        })
+        <div>
+          <div className={styles.inProgress}>
+            {ordersInProgress.map((order) => {
+              return <OrdersInProgress order={order} />;
+            })}
+          </div>
+          <div className={styles.completed}>
+            {completedOrders.map((order) => {
+              return <CompletedOrder order={order} />;
+            })}
+          </div>
+        </div>
       ) : (
         <h1 className={styles.noOrdersTitle}>
           У вас немає замовлень :(
