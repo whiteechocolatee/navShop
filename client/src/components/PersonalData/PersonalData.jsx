162c970d827f59c9828ec4df23f0092d909cdbe2
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-05 12:00:10 +0300

    inser loading to personal data component

--- client/src/components/PersonalData/PersonalData.jsx
+++ client/src/components/PersonalData/PersonalData.jsx
@@ -4,35 +4,39 @@ import { useSelector } from "react-redux";
 import { Button } from "../Button/Button";
 import { Link } from "react-router-dom";
 import { paths } from "../../paths";
+import { Loader } from "../Loader/Loader";
 
 export const PersonalData = () => {
-  const { user } = useSelector(
+  const { user, isLoading } = useSelector(
     (state) => state.userAuthReducer,
   );
 
-  const handleChange = () => {
-    console.log(1);
-  };
-
   return (
-    <div className={styles.personalBlock}>
-      <div className={styles.personalData}>
-        <span>{user.name} {user.surname}</span>
-        <span>{user.email}</span>
-        <span>{user.phoneNumber}</span>
-        <Button
-          onClick={handleChange}
-          containerClassName={styles.btn}
-          children={
-            <Link
-              className={styles.link}
-              to={`${paths.account}${paths.changeUserData}`}>
-              Змінити
-            </Link>
-          }
-        />
-      </div>
-      {/* map user addresses   */}
+    <div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className={styles.personalBlock}>
+          <div className={styles.personalData}>
+            <span>
+              {user.name} {user.surname}
+            </span>
+            <span>{user.email}</span>
+            <span>{user.phoneNumber}</span>
+            <Button
+              containerClassName={styles.btn}
+              children={
+                <Link
+                  className={styles.link}
+                  to={`${paths.account}${paths.changeUserData}`}>
+                  Змінити
+                </Link>
+              }
+            />
+          </div>
+          {/* map user addresses   */}
+        </div>
+      )}
     </div>
   );
 };
