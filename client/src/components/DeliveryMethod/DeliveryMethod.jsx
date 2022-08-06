30d38b9b6f720420f43dc2392998751c50b9f6eb
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-06 18:40:54 +0300

    include checkbox component and delete styles for old checkboxes

--- client/src/components/DeliveryMethod/DeliveryMethod.jsx
+++ client/src/components/DeliveryMethod/DeliveryMethod.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Checkbox } from "../Checkbox/Checkbox";
 import { Courier } from "../CourierDelivery/Courier";
 import { Map } from "../MapContainer/Map";
 import { NovaPoshta } from "../NovaPoshta/NovaPoshta";
@@ -12,54 +13,42 @@ export const DeliveryMethod = ({
     <div className={styles.deliveryMethod}>
       <h4 className={styles.deliveryTitle}>Доставка</h4>
       <div className={styles.methods}>
-        <div className={styles.label}>
-          <input
-            name='shippingMethod'
-            type='radio'
-            value='localPickup'
-            checked={
-              values.shippingMethod === "localPickup"
-            }
-            onChange={handleChange}
-            id='localPickup'
-          />
-          <label htmlFor='localPickup'>Самовивіз</label>
-        </div>
-        <div className={styles.label}>
-          <input
-            name='shippingMethod'
-            id='nova'
-            type='radio'
-            value='novaPoshta'
-            onChange={handleChange}
-            checked={values.shippingMethod === "novaPoshta"}
-          />
-          <label htmlFor='nova'>
-            Доставка Новою Поштою
-          </label>
-        </div>
-        <div className={styles.label}>
-          <input
-            name='shippingMethod'
-            id='poshta'
-            type='radio'
-            value='poshta'
-            onChange={handleChange}
-            checked={values.shippingMethod === "poshta"}
-          />
-          <label htmlFor='poshta'>Доставка поштою</label>
-        </div>
-        <div className={styles.label}>
-          <input
-            name='shippingMethod'
-            id='courier'
-            type='radio'
-            value='courier'
-            onChange={handleChange}
-            checked={values.shippingMethod === "courier"}
-          />
-          <label htmlFor='courier'>Доставка кур'єром</label>
-        </div>
+        <Checkbox
+          onChange={handleChange}
+          name='shippingMethod'
+          checkedValue={
+            values.shippingMethod === "localPickup"
+          }
+          value='localPickup'
+          children='Самовивіз'
+          containerClassName={styles.checkbox}
+        />
+        <Checkbox
+          onChange={handleChange}
+          name='shippingMethod'
+          checkedValue={
+            values.shippingMethod === "novaPoshta"
+          }
+          value='novaPoshta'
+          children='Доставка Новою Поштою'
+          containerClassName={styles.checkbox}
+        />
+        <Checkbox
+          onChange={handleChange}
+          name='shippingMethod'
+          checkedValue={values.shippingMethod === "poshta"}
+          value='poshta'
+          children='Доставка поштою'
+          containerClassName={styles.checkbox}
+        />
+        <Checkbox
+          onChange={handleChange}
+          name='shippingMethod'
+          checkedValue={values.shippingMethod === "courier"}
+          value='courier'
+          children="Доставка кур'єром"
+          containerClassName={styles.checkbox}
+        />
       </div>
       <div className={styles.wayOfMethod}>
         {values.shippingMethod === "localPickup" && (
