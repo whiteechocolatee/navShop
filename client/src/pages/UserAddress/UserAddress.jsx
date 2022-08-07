4bc1ce9e0a8969177ed0508f11b091b74da8ee96
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-07 18:39:41 +0300

    implement request for users while they are adding addresses

--- client/src/pages/UserAddress/UserAddress.jsx
+++ client/src/pages/UserAddress/UserAddress.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import { CategoriesNavigation } from "../../components/CategoriesNav/CategoriesNavigation";
 import { Header } from "../../components/Header/Header";
 import { ContentWrapper } from "../../components/contentWrapper/ContentWrapper";
@@ -6,17 +8,24 @@ import { UserAccount } from "../../components/UserAccount/UserAccount";
 import { Footer } from "../../components/Footer/Footer";
 import { UserAddAddress } from "../../components/UserAddAddress/UserAddAddress";
 
+import { userProfile } from "../../store/users/userAuthSlice";
+import { getUserOrders } from "../../store/order/orderSlice";
+
 export const UserAddress = () => {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(userProfile());
+    dispatch(getUserOrders());
+  }, [dispatch]);
 
-  
   return (
     <React.Fragment>
       <Header />
       <CategoriesNavigation />
       <ContentWrapper>
         <UserAccount />
-        <UserAddAddress/>
+        <UserAddAddress />
       </ContentWrapper>
       <Footer />
     </React.Fragment>
