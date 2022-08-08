b7ab091a0dc2ef4398cbd73f94cd49850f8aa23b
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-08 21:17:03 +0300

    delete user address form and insert like component with props

--- client/src/components/UserAddAddress/UserAddAddress.jsx
+++ client/src/components/UserAddAddress/UserAddAddress.jsx
@@ -1,20 +1,7 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { UserAddressForm } from "../UserAddressForm/UserAddressForm";
 
-import styles from "./address.module.css";
-import { NovaPoshta } from "../NovaPoshta/NovaPoshta";
-import { Input } from "../Input/Input";
-import { Button } from "../Button/Button";
-
-import { paths } from "../../paths";
-import { useDispatch, useSelector } from "react-redux";
-import { saveAddress } from "../../store/users/userAuthSlice";
-import { Checkbox } from "../Checkbox/Checkbox";
-
-export const UserAddAddress = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
+export const UserAddAddress = ({ handleAddress }) => {
   const [values, setValues] = useState({
     area: "Виберіть область",
     city: "Виберіть місто",
@@ -24,39 +11,6 @@ export const UserAddAddress = () => {
     main: "",
   });
 
-  const { user } = useSelector(
-    (state) => state.userAuthReducer,
-  );
-
-  const handleAddress = () => {
-    if (user.addresses.length === 3) {
-      navigate(paths.account);
-      return;
-    }
-    let addresses = [...user.addresses];
-    if (values.main === "yes") {
-      addresses = addresses.map((address) => {
-        if (address.main === "yes") {
-          return { ...address, main: "no" };
-        }
-        return address;
-      });
-      addresses.push(values);
-      dispatch(saveAddress(addresses)).then((res) => {
-        if (res.payload) {
-          navigate(paths.account);
-        }
-      });
-    } else {
-      addresses.push(values);
-      dispatch(saveAddress(addresses)).then((res) => {
-        if (res.payload) {
-          navigate(paths.account);
-        }
-      });
-    }
-  };
-
   const handleChange = (e) => {
     setValues({
       ...values,
@@ -64,82 +18,13 @@ export const UserAddAddress = () => {
     });
   };
 
-  const inputs = [
-    {
-      id: 1,
-      name: "index",
-      type: "text",
-      placeholder: "Вкажіть індекс",
-      errorMessage:
-        "Не повинно мати літери або символи, максимальна довжина 5 символів.",
-      pattern: "^[0-9]{5}$",
-      required: true,
-    },
-    {
-      id: 2,
-      name: "street",
-      type: "text",
-      placeholder: "Вкажіть вулицю",
-      errorMessage:
-        "Не повинно мати цифри або символи, максимальна довжина 25 символів.",
-      pattern: "^[ A-Za-zА-ЩЬЮЯҐЄІЇа-щьюяґєії0-9]{3,30}$",
-      required: true,
-    },
-  ];
-
   return (
-    <div className={styles.addressBlock}>
-      <div className={styles.addressForm}>
-        <div className={styles.columns}>
-          <NovaPoshta
-            values={values}
-            handleChange={handleChange}
-          />
-        </div>
-        <div
-          className={`${styles.columns} ${styles.inputs} `}>
-          {inputs.map((input) => (
-            <div className={styles.dataBlock}>
-              <Input
-                key={input.id}
-                {...input}
-                className={styles.input}
-                value={values[input.name]}
-                onChange={handleChange}
-              />
-            </div>
-          ))}
-          <div className={styles.isMain}>
-            <p className={styles.isMainTitle}>
-              Зробити основним?
-            </p>
-            <Checkbox
-              name={"main"}
-              onChange={handleChange}
-              value={"yes"}
-              checkedValue={values.main === "yes"}
-              children={"Так"}
-            />
-            <Checkbox
-              name={"main"}
-              onChange={handleChange}
-              value={"no"}
-              checkedValue={values.main === "no"}
-              children={"Ні"}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles.navigation}>
-        <Link className={styles.link} to={paths.account}>
-          Назад
-        </Link>
-        <Button
-          onClick={handleAddress}
-          containerClassName={styles.btn}
-          children='Зберегти'
-        />
-      </div>
-    </div>
+    <>
+      <UserAddressForm
+        handleChange={handleChange}
+        handleAddress={handleAddress}
+        values={values}
+      />
+    </>
   );
 };
