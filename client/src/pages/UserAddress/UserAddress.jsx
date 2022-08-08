57bb2a79e69242aeaa53cba3569241031cc9b2f6
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-08 21:17:58 +0300

    insert main logic to user addresses page

--- client/src/pages/UserAddress/UserAddress.jsx
+++ client/src/pages/UserAddress/UserAddress.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { paths } from "../../paths";
+import { useDispatch, useSelector } from "react-redux";
+import { saveAddress } from "../../store/users/userAuthSlice";
 
 import { CategoriesNavigation } from "../../components/CategoriesNav/CategoriesNavigation";
 import { Header } from "../../components/Header/Header";
@@ -13,6 +16,40 @@ import { getUserOrders } from "../../store/order/orderSlice";
 
 export const UserAddress = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user } = useSelector(
+    (state) => state.userAuthReducer,
+  );
+
+  const handleAddress = (values) => {
+    if (user.addresses.length === 3) {
+      navigate(paths.account);
+      return;
+    }
+    let addresses = [...user.addresses];
+    if (values.main === "yes") {
+      addresses = addresses.map((address) => {
+        if (address.main === "yes") {
+          return { ...address, main: "no" };
+        }
+        return address;
+      });
+      addresses.push(values);
+      dispatch(saveAddress(addresses)).then((res) => {
+        if (res.payload) {
+          navigate(paths.account);
+        }
+      });
+    } else {
+      addresses.push(values);
+      dispatch(saveAddress(addresses)).then((res) => {
+        if (res.payload) {
+          navigate(paths.account);
+        }
+      });
+    }
+  };
 
   useEffect(() => {
     dispatch(userProfile());
@@ -25,7 +62,7 @@ export const UserAddress = () => {
       <CategoriesNavigation />
       <ContentWrapper>
         <UserAccount />
-        <UserAddAddress />
+        <UserAddAddress handleAddress={handleAddress} />
       </ContentWrapper>
       <Footer />
     </React.Fragment>
