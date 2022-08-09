9c244f04d846006066fbc7cf680b9e25e6dca265
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-09 23:06:57 +0300

    create route for removing items from favorites

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const {
   updateProfile,
   addAddress,
   addToFavorite,
-  getUserFavorites,
+  removeFromFavorite,
 } = require("../controllers/userControllers");
 const protection = require("../utils/authMiddleware");
 
@@ -18,5 +18,10 @@ router.get("/profile", protection, userProfile);
 router.put("/profile", protection, updateProfile);
 router.put("/profile/address", protection, addAddress);
 router.put("/profile/favorite", protection, addToFavorite);
+router.put(
+  "/profile/favorite/remove",
+  protection,
+  removeFromFavorite,
+);
 
 module.exports = router;
