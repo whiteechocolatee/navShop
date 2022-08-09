09ee0280e65986eb7815a88704410aa25f23f700
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-09 18:15:07 +0300

    insert popup window , add ability to add items to favorites

--- client/src/components/ItemCard/Item.jsx
+++ client/src/components/ItemCard/Item.jsx
@@ -1,20 +1,22 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import styles from "./item.module.css";
-import { FaRegHeart } from "react-icons/fa";
 import { Button } from "../Button/Button";
-
-import { Link } from "react-router-dom";
 import { paths } from "../../paths";
 import { ImageComponent } from "../Image/Image";
+import { setItemInCart } from "../../store/cart/cartSlice";
+import { Popup } from "../Popup/Popup";
 import {
-  setItemInCart,
-} from "../../store/cart/cartSlice";
+  checkIsAuth,
+  addToFavorite,
+} from "../../store/users/userAuthSlice";
 
 export const Item = ({ item }) => {
   const dispatch = useDispatch();
+  const [buttonPopup, setButtonPopup] = useState(false);
 
   const { _id, title, price, itemImage, discount } = item;
 
@@ -22,9 +24,19 @@ export const Item = ({ item }) => {
     (state) => state.cartReducer.itemsInCart,
   );
 
+  const favorites = useSelector(
+    (state) => state.userAuthReducer.favorites,
+  );
+
+  const isAuth = useSelector(checkIsAuth);
+
   const isItemInCart = cart.some(
     (itemInCart) => itemInCart._id === item._id,
   );
+
+  const isItemFavorite = favorites.some((favoriteItem) => {
+    return favoriteItem._id === item._id;
+  });
 
   const addToCart = (e) => {
     e.stopPropagation();
@@ -36,28 +48,79 @@ export const Item = ({ item }) => {
     }
   };
 
+  const addToFavoriteFunc = () => {
+    if (isAuth) {
+      dispatch(addToFavorite({ product: _id }));
+    } else {
+      setButtonPopup(true);
+    }
+  };
+
   return (
     <div className={styles.cardBody}>
-      <Link
-        className={styles.itemLink}
-        to={`${paths.itemPage}/${_id}`}>
-        <div className={styles.cardImageBlock}>
-          <div className={styles.addToFavorite}>
-            <div
-              className={`${
-                discount > 0 ? styles.sale : styles.saleNone
-              }`}>
-              {discount} %
-            </div>
-            <FaRegHeart />
+      <div className={styles.cardImageBlock}>
+        <div className={styles.addToFavorite}>
+          <div
+            className={`${
+              discount > 0 ? styles.sale : styles.saleNone
+            }`}>
+            {discount} %
           </div>
+          {isItemFavorite ? (
+            <div
+              // onClick={addToFavoriteFunc}
+              className={styles.svgBg}>
+              <FaHeart />
+            </div>
+          ) : (
+            <div
+              onClick={addToFavoriteFunc}
+              className={styles.svg}>
+              <FaRegHeart />
+            </div>
+          )}
+          {/* <div className={styles.svg}>
+              <FaRegHeart />
+            </div> */}
+          {/* <div
+            onClick={addToFavoriteFunc}
+            className={styles.svgBg}>
+            <FaHeart />
+          </div> */}
+          <Popup
+            setTrigger={setButtonPopup}
+            trigger={buttonPopup}>
+            <div className={styles.popupContent}>
+              <h1 className={styles.popupTitle}>
+                Додати у бажане
+              </h1>
+              <p className={styles.popupText}>
+                Для того щоб додати товар у бажане, вам
+                потрібно увійти в акаунт або створити його.
+                Тоді усі товари будуть збережені в акаунті
+              </p>
+              <Button
+                onClick={() => setButtonPopup(false)}
+                containerClassName={styles.popupBtn}>
+                Зрозуміло
+              </Button>
+            </div>
+          </Popup>
+        </div>
+        <Link
+          className={styles.itemLink}
+          to={`${paths.itemPage}/${_id}`}>
           <div className={styles.cardImg}>
             <ImageComponent
               publicId={itemImage}
               alt={title}
             />
           </div>
-        </div>
+        </Link>
+      </div>
+      <Link
+        className={styles.itemLink}
+        to={`${paths.itemPage}/${_id}`}>
         <div className={styles.description}>{title}</div>
       </Link>
       <div className={styles.addToCart}>
