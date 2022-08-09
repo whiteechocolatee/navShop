e2981c2f2618f21750c6a61b2ca8c69b0b8a4d63
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-09 23:07:48 +0300

    create controller for removing items by id from favorites

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -188,6 +188,10 @@ const updateProfile = async (req, res) => {
       if (req.body.password) {
         user.password = req.body.password;
       }
+    } else {
+      res
+        .status(404)
+        .json({ message: "Користувач не знайден!" });
     }
 
     const updatedUser = await user.save();
@@ -224,6 +228,10 @@ const addAddress = async (req, res) => {
 
     if (user) {
       user.addresses = address || user.addresses;
+    } else {
+      res
+        .status(404)
+        .json({ message: "Користувач не знайден!" });
     }
 
     const updatedUser = await user.save();
@@ -256,8 +264,6 @@ const addToFavorite = async (req, res) => {
   try {
     const favoriteItem = req.body;
 
-    console.log(favoriteItem);
-
     const user = await User.findById(req.user._id);
 
     if (user) {
@@ -268,6 +274,10 @@ const addToFavorite = async (req, res) => {
       );
 
       if (alreadyFavorite) return res.status(400);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Користувач не знайден!" });
     }
 
     user.favorites.push(favoriteItem);
@@ -304,6 +314,67 @@ const addToFavorite = async (req, res) => {
   }
 };
 
+/**
+ * It removes a product from the user's favorites
+ * @param req - The request object. This contains information about the HTTP request that raised the
+ * event.
+ * @param res - the response object
+ */
+const removeFromFavorite = async (req, res) => {
+  try {
+    const favoriteToRemove = req.body;
+
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      const favorites = user.favorites;
+
+      const editedFavorites = favorites.filter(
+        (favorite) => {
+          return (
+            favorite.product.toString() !==
+            favoriteToRemove.product.toString()
+          );
+        },
+      );
+
+      user.favorites = editedFavorites;
+
+      const updatedUser = await user.save();
+
+      const ids = updatedUser.favorites.map((item) => {
+        return item.product;
+      });
+
+      const records = await Item.find()
+        .where("_id")
+        .in(ids)
+        .exec();
+
+      return res.json({
+        _id: user._id,
+        name: user.name,
+        surname: user.surname,
+        phoneNumber: user.phoneNumber,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        createdAt: user.createdAt,
+        favorites: records,
+        addresses: user.addresses,
+        token: generateToken(user._id),
+      });
+    } else {
+      res
+        .status(404)
+        .json({ message: "Користувач не знайден!" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Щось пішло не так, зверніться пізніше",
+    });
+  }
+};
+
 module.exports = {
   addAddress,
   updateProfile,
@@ -311,4 +382,5 @@ module.exports = {
   userRegistration,
   userProfile,
   addToFavorite,
+  removeFromFavorite,
 };
