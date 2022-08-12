a722615e5c81b45d75f2cdc8c797191e89608aae
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-12 18:36:38 +0300

    Update Filter.jsx

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -3,27 +3,26 @@ import styles from "./filter.module.css";
 import { BsPlusLg } from "react-icons/bs";
 import { HiMinus } from "react-icons/hi";
 
-export const Filter = ({ data }) => {
+export const Filter = ({ characteristic }) => {
   const [selected, setSelected] = useState(null);
 
   const toggle = (i) => {
     if (selected === i) {
       return setSelected(null);
     }
-
     setSelected(i);
   };
 
   return (
     <div className={styles.accordion}>
-      {data.map((item, index) => (
+      {characteristic?.map((item) => (
         <div className={styles.item}>
           <div
             className={styles.title}
-            onClick={() => toggle(index)}>
-            <span>{item.question}</span>
+            onClick={() => toggle(item._id)}>
+            <span>{item.name}</span>
             <span>
-              {selected === index ? (
+              {selected === item._id ? (
                 <HiMinus />
               ) : (
                 <BsPlusLg />
@@ -32,11 +31,13 @@ export const Filter = ({ data }) => {
           </div>
           <div
             className={
-              selected === index
+              selected === item._id
                 ? `${styles.content} ${styles.show}`
                 : styles.content
             }>
-            {item.answer}
+            {item?.description?.map((text) => (
+              <p>{text}</p>
+            ))}
           </div>
         </div>
       ))}
