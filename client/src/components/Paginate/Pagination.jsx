2c55c5f8295900b26597aeb82db14accf4a90c15
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-16 13:52:12 +0300

    change pagintaion logic for more paegs

--- client/src/components/Paginate/Pagination.jsx
+++ client/src/components/Paginate/Pagination.jsx
@@ -2,42 +2,87 @@
 import React from "react";
 import styles from "./style.module.css";
 
+import {
+  IoIosArrowBack,
+  IoIosArrowForward,
+} from "react-icons/io";
+
 export const Pagination = ({
   itemsPerPage,
   totalItems,
   paginate,
   currentPage,
 }) => {
-  const pageNumbers = [];
-
+  let maxPages = Math.ceil(totalItems / itemsPerPage);
+  let items = [];
+  let leftSide = currentPage - 2;
+  if (leftSide <= 0) leftSide = 1;
+  let rightSide = currentPage + 2;
+  if (rightSide > maxPages) rightSide = maxPages;
   for (
-    let i = 1;
-    i <= Math.ceil(totalItems / itemsPerPage);
-    i++
+    let number = leftSide;
+    number <= rightSide;
+    number++
   ) {
-    pageNumbers.push(i);
+    items.push(
+      <li
+        className={`${styles.li} ${
+          currentPage === number ? styles.active : ""
+        }`}
+        key={number}>
+        <a
+          className={`${styles.a}`}
+          onClick={() => {
+            paginate(number);
+          }}>
+          {number < 10 ? "0" + number : number}
+        </a>
+      </li>,
+    );
   }
+
+  const nextPage = () => {
+    if (currentPage < maxPages) {
+      paginate(currentPage + 1);
+    }
+    window.scroll(0, 0);
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      paginate(currentPage - 1);
+    }
+    window.scroll(0, 0);
+  };
 
   return (
     <div className={styles.paginateContainer}>
       <ul className={styles.ul}>
-        {pageNumbers.map((number) => {
-          return (
-            <li
-              className={`${styles.li} ${
-                currentPage === number ? styles.active : ""
-              }`}
-              key={number}>
-              <a
-                className={`${styles.a}`}
-                onClick={() => {
-                  paginate(number);
-                }}>
-                {number < 10 ? "0" + number : number}
-              </a>
-            </li>
-          );
-        })}
+        <li
+          className={` ${
+            maxPages < 5 ? styles.hide : styles.li
+          }`}>
+          <a
+            className={`${styles.a}`}
+            onClick={() => {
+              prevPage();
+            }}>
+            <IoIosArrowBack />
+          </a>
+        </li>
+        {items}
+        <li
+          className={` ${
+            maxPages < 5 ? styles.hide : styles.li
+          }`}>
+          <a
+            className={`${styles.a}`}
+            onClick={() => {
+              nextPage();
+            }}>
+            <IoIosArrowForward />
+          </a>
+        </li>
       </ul>
     </div>
   );
