b20cb56d23e06376ff6c3c0ffd8b8f14a79f4147
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-20 19:17:44 +0300

    change layout

--- client/src/components/PersonalData/PersonalData.jsx
+++ client/src/components/PersonalData/PersonalData.jsx
@@ -25,61 +25,78 @@ export const PersonalData = ({
       {isLoading ? (
         <Loader containerClassName={styles.loader} />
       ) : (
-        <div className={styles.personalBlock}>
-          <div className={styles.personalData}>
-            <div>
-              <p>
-                {user.name} {user.surname}
-              </p>
-              <p>{user.email}</p>
-              <p>{user.phoneNumber}</p>
-            </div>
-            <Button
-              containerClassName={styles.btn}
-              children={
-                <Link
-                  className={styles.link}
-                  to={`${paths.account}${paths.changeUserData}`}>
-                  Змінити
-                </Link>
-              }
-            />
-          </div>
-          <div className={styles.addressesContainer}>
-            {user?.addresses?.length < 3 ? (
-              <div className={styles.addressesWithBtn}>
-                {user?.addresses?.map((address) => (
-                  <UserAddressBlock
-                    handleDelete={handleDelete}
-                    changeMain={changeMain}
-                    key={address?._id}
-                    address={address}
-                  />
-                ))}
-                <div className={styles.addAddress}>
-                  <p>Додати адресу</p>
-                  <div
-                    onClick={addAddress}
-                    className={styles.icon}>
-                    <div className={styles.outline}>
-                      <FiPlus />
-                    </div>
+        <div className={styles.addressesContainer}>
+          {user?.addresses?.length < 3 ? (
+            <div className={styles.addressesWithBtn}>
+              <div className={styles.personalData}>
+                <div>
+                  <p>
+                    {user.name} {user.surname}
+                  </p>
+                  <p>{user.email}</p>
+                  <p>{user.phoneNumber}</p>
+                </div>
+                <Button
+                  containerClassName={styles.btn}
+                  children={
+                    <Link
+                      className={styles.link}
+                      to={`${paths.account}${paths.changeUserData}`}>
+                      Змінити
+                    </Link>
+                  }
+                />
+              </div>
+              {user?.addresses?.map((address) => (
+                <UserAddressBlock
+                  handleDelete={handleDelete}
+                  changeMain={changeMain}
+                  key={address?._id}
+                  address={address}
+                />
+              ))}
+              <div className={styles.addAddress}>
+                <p>Додати адресу</p>
+                <div
+                  onClick={addAddress}
+                  className={styles.icon}>
+                  <div className={styles.outline}>
+                    <FiPlus />
                   </div>
                 </div>
               </div>
-            ) : (
-              <div className={styles.addresses}>
-                {user?.addresses?.map((address) => (
-                  <UserAddressBlock
-                    handleDelete={handleDelete}
-                    changeMain={changeMain}
-                    key={address._id}
-                    address={address}
-                  />
-                ))}
+            </div>
+          ) : (
+            <div className={styles.addresses}>
+              <div className={styles.personalData}>
+                <div>
+                  <p>
+                    {user.name} {user.surname}
+                  </p>
+                  <p>{user.email}</p>
+                  <p>{user.phoneNumber}</p>
+                </div>
+                <Button
+                  containerClassName={styles.btn}
+                  children={
+                    <Link
+                      className={styles.link}
+                      to={`${paths.account}${paths.changeUserData}`}>
+                      Змінити
+                    </Link>
+                  }
+                />
               </div>
-            )}
-          </div>
+              {user?.addresses?.map((address) => (
+                <UserAddressBlock
+                  handleDelete={handleDelete}
+                  changeMain={changeMain}
+                  key={address._id}
+                  address={address}
+                />
+              ))}
+            </div>
+          )}
         </div>
       )}
     </>
