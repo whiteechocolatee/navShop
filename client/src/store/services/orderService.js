01b0694392942eb7a1bbcafd3c7e36d4e658b464
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-23 18:18:19 +0300

    It gets all the orders from the database  @returns An array of objects.

--- client/src/store/services/orderService.js
+++ client/src/store/services/orderService.js
@@ -53,9 +53,33 @@ const createOrder = async ({
   return response.data;
 };
 
+/**
+ * It gets all the orders from the database
+ * @returns An array of objects.
+ */
+const getOrders = async () => {
+  const token = JSON.parse(
+    window.localStorage.getItem("token"),
+  );
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  let { data } = await axios.get(
+    "/api/admin/orders",
+    config,
+  );
+
+  return data;
+};
+
 const orderService = {
   createOrder,
   userOrders,
+  getOrders,
 };
 
 export default orderService;
