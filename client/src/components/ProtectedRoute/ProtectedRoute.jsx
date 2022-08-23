bc20ee7f5bd1a9a310f87b32eea6a4162dfed25e
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-23 18:13:39 +0300

    add condition like props

--- client/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { checkIsAuth } from "../../store/users/userAuthSlice";
+
 import { Login } from "../../pages/Login/Login";
 
-export const ProtectedRoute = () => {
-  const isAuth = useSelector(checkIsAuth);
-  return isAuth ? <Outlet /> : <Login />;
+export const ProtectedRoute = ({ condition }) => {
+  return condition ? <Outlet /> : <Login />;
 };
