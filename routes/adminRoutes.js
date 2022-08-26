a42d1f2b1501f4033200adf4f80482ff8e4da72c
Andrey Chorniy <creatuseandr@icloud.com>
2022-08-26 17:56:46 +0300

    add new route for getting all users

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,8 +7,11 @@ const {
 } = require("../controllers/itemControllers");
 const {
   getOrders,
-  markAsDelivered,
 } = require("../controllers/orderController");
+const {
+  getUsers,
+} = require("../controllers/userControllers");
+
 const protection = require("../utils/authMiddleware");
 
 const router = express.Router();
@@ -17,6 +20,6 @@ router
   .post("/createItem", createItem)
   .get("/orders", protection, getOrders)
   .get("/callbacks", protection, getCallbacks)
-
+  .get("/users", protection, getUsers);
 
 module.exports = router;
